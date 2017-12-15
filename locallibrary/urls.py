5243487bf35e7a7d800cd80b1b7cffb5be29fd19
Pin Cheng <pcheng@rocketsoftware.com>
2017-12-15 16:29:18 +0800

    add default and static routes

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -15,7 +15,23 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.conf.urls import include
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns += [
+    url(r'^catalog/', include('catalog.urls')),
+]
+
+urlpatterns += [
+    url(r'^S', RedirectView.as_view(url='/catalog/', permanent=True))
+]
+
+# Use static() to add url mapping to serve static files during development only
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
